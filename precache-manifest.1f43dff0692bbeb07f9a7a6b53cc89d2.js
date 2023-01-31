3bdc88b5c64744e81084144f0b183fc920c89b14
Spark <46791876+lolmyprogrammer@users.noreply.github.com>
2023-01-31 15:29:55 +0100

    Updates

---
+++ precache-manifest.1f43dff0692bbeb07f9a7a6b53cc89d2.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8355247e600848b638515268d16003b6",
+    "url": "/ForM/index.html"
+  },
+  {
+    "revision": "4fc1da49a560f0118826",
+    "url": "/ForM/static/css/main.a5dce85a.chunk.css"
+  },
+  {
+    "revision": "cf3b14334f84f5ece0fa",
+    "url": "/ForM/static/js/2.52431200.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/ForM/static/js/2.52431200.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4fc1da49a560f0118826",
+    "url": "/ForM/static/js/main.18afb313.chunk.js"
+  },
+  {
+    "revision": "8717bc4d2d96f5cbc4f5",
+    "url": "/ForM/static/js/runtime-main.10e89bbd.js"
+  },
+  {
+    "revision": "c059909345bd4cafde71917c21494432",
+    "url": "/ForM/static/media/Ceskam.c0599093.gif"
+  },
+  {
+    "revision": "7746f73129cc8fc7f46ada5e23f4d1b5",
+    "url": "/ForM/static/media/Inaet.7746f731.gif"
+  },
+  {
+    "revision": "e831c6c0da241c5146771d0233cfc219",
+    "url": "/ForM/static/media/Interesno.e831c6c0.gif"
+  },
+  {
+    "revision": "46feb314ef66fd9844e6c5343e228597",
+    "url": "/ForM/static/media/Lutka.46feb314.gif"
+  },
+  {
+    "revision": "d348e4401884dd1bb421bbe513fa54a7",
+    "url": "/ForM/static/media/Mila.d348e440.gif"
+  },
+  {
+    "revision": "04b613c4523e12dcb35d7f703a752faf",
+    "url": "/ForM/static/media/MilaBojan.04b613c4.gif"
+  },
+  {
+    "revision": "641a584c05345c733e8e5d94f906e53c",
+    "url": "/ForM/static/media/TeSakam.641a584c.gif"
+  },
+  {
+    "revision": "f1be026ff08aab1125b691c069d382f0",
+    "url": "/ForM/static/media/Valentine.f1be026f.gif"
+  },
+  {
+    "revision": "8b0c4eecc314fa3131eae4e8ec70d06a",
+    "url": "/ForM/static/media/Valorant.8b0c4eec.png"
+  },
+  {
+    "revision": "f1ce781bc0d12e9285679d2a731b3278",
+    "url": "/ForM/static/media/WOO.f1ce781b.gif"
+  },
+  {
+    "revision": "ee3bbcefdd762dc554b5db84df32bded",
+    "url": "/ForM/static/media/ZaMene.ee3bbcef.gif"
+  },
+  {
+    "revision": "865f1c977c2083c42cda7c223f72ea76",
+    "url": "/ForM/static/media/ZaTebe.865f1c97.gif"
+  }
+]);
